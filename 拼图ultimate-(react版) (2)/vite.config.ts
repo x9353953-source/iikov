d44b53b330f67d0962a38034c302a8f417fda9d6
ikk12 <x9353953@gmail.com>
2026-01-21 14:03:30 +0800

    Update vite.config.ts

--- 拼图ultimate-(react版) (2)/vite.config.ts
+++ 拼图ultimate-(react版) (2)/vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
+    
     const env = loadEnv(mode, '.', '');
-    return {
+    return {base: './',  // <--- 必须加这一行，注意有个逗号
       server: {
         port: 3000,
         host: '0.0.0.0',
